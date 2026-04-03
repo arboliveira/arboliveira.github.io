20f9b083c6491c1fd21586a6ffe71f419b84fb89
André de Oliveira <andre@arbo.com.br>
2026-04-03 17:27:23 -0600

    https://arboliveira.github.io/

---
+++ index.html.test.js
@@ -0,0 +1,45 @@
+import * as chai from "chai";
+import 'mocha';
+import * as CT from "#customary-testing";
+const suite = {
+    title: 'arbo.com.br',
+    subject_html: 'index.html'
+};
+describe(suite.title, async function () {
+    this.timeout(4000);
+    this.slow(500);
+    let window;
+    before(() => window = CT.open(suite.subject_html));
+    after(() => window.close());
+    describe('happy day', async function () {
+        let element;
+        let logo_circle;
+        function assert_element() {
+            element = CT.querySelector('the-arbo-group-logo', window);
+        }
+        function assert_spinning(spinning) {
+            chai.assert.equal(logo_circle.classList.contains('wheel-of-fortune-spinning'), spinning);
+        }
+        it('looks good', async function () {
+            this.retries(64);
+            assert_element();
+            logo_circle = CT.querySelector('#logo-circle', element);
+            assert_spinning(false);
+        });
+        it('interact', async function () {
+            logo_circle.click();
+        });
+        it('looks good', async function () {
+            this.retries(16);
+            assert_spinning(true);
+        });
+        it('interact', async function () {
+            logo_circle.click();
+        });
+        it('looks good', async function () {
+            this.retries(16);
+            assert_spinning(false);
+        });
+    });
+});
+//# sourceMappingURL=index.html.test.js.map
